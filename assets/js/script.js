3f61b9820207628965902fc29adb9cdc26ee5789
Josh8903 <thorngren87@gmail.com>
2020-11-17 19:52:44 -0600

    changed quack noise to meow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
-  $(".quacky").click(function () {
-    var audio = $("#quack")[0];
-    if (audio.paused) {
-      audio.play();
-      $(this).attr("id", "true");
-    } else {
-      audio.pause();
-      $(this).attr("id", "false");
-    }
+  $(".meow").click(function () {
+    var audio = $("#meow")[0];
+    audio.play();
   });
 });
+
+// play/pause code - antispam
+// $(document).ready(function () {
+//   $(".quacky").click(function () {
+//     var audio = $("#quack")[0];
+//     if (audio.paused) {
+//       audio.play();
+//       $(this).attr("id", "true");
+//     } else {
+//       audio.pause();
+//       $(this).attr("id", "false");
+//     }
+//   });
+// });
